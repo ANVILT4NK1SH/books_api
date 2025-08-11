2455871d21956f56b2d30ac0d1580e02c030022f
iamsh <iamshawnhartline@gmail.com>
2025-08-10 19:47:57 -0500

    Need to add Cloudinary support.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,13 @@
 class Book < ApplicationRecord
+  include Rails.application.routes.url_helpers
   belongs_to :user
+  has_one_attached :cover_image
+
   validates :title, presence: true
   validates :author, presence: true
   validates :read, inclusion: { in: [ true, false ] }
+
+  def cover_image_url
+    rails_blob_url(self.cover_image, only_path: false) if self.cover_image.attached?
+  end
 end
